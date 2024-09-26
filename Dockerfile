e9752ff15d50ebd0250a8e8885f56e10def773c3
vishnudxb <vishnudxb@protonmail.com>
2024-09-26 17:06:12 -0400

    update port

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ COPY app.py /app/app.py
 WORKDIR /app
 
 # Expose port 8080
-EXPOSE 80
+EXPOSE 8080
 
 # Run the Flask app
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def expose_token():
     return jsonify(token)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8080)
 
